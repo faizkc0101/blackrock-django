b4b4200af6582a4e90be1fa903b8f87880035717
faizkc0101 <kcblack0101@gmail.com>
2024-10-05 16:11:08 +0530

    oct 5 add cart/search  now good fisr view

--- blackrock/settings.py
+++ blackrock/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'carts',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',
+                'carts.context_processors.counter',
             ],
         },
     },
--- store/models.py
+++ store/models.py
@@ -3,7 +3,7 @@ from category.models import Category
 from django.urls import reverse
 
 # Create your models here.
-class product(models.Model):
+class Product(models.Model):
     product_name  = models.CharField(max_length=200,unique=True)
     slug          = models.SlugField(max_length=200,unique=True)
     description   = models.TextField(max_length=500,blank=True)
@@ -20,4 +20,5 @@ class product(models.Model):
 
     def __str__(self):
         return self.product_name
+
     
